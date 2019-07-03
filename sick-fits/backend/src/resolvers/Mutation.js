86867d578471be228904f54e59b851e38febf1fe
Teddy Otieno <asolatotieno@gmail.com>
2019-07-03 03:40:31 +0300

    add update item functionality backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,19 @@ const Mutation = {
       info
     );
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,8 @@
+const { forwardTo } = require("prisma-binding");
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items();
-    return items;
-  }
+  items: forwardTo("db"),
+  item: forwardTo("db")
 };
 
 module.exports = Query;
